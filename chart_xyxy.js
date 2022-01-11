e99c62a4da9bfb64c30cb332f725715fd7ea0598
Dušan Halický <dusan.halicky@gmail.com>
2022-01-11 13:39:56 +0100

    Logarithmic chart support

--- chart_xyxy.js
+++ chart_xyxy.js
@@ -11,9 +11,10 @@ SC.palette = ['#ff0000', '#00ff00', '#0077ff', '#ffff00', '#ff00ff', '#00ffff'];
 SC.ChartXYXY = function (aCanvasOrId) {
     //var pan, p1, p2, t = this, rec;
     var t = this, pan, rec, p1, p2;
+    this.logY = false;
     this.series = [];
     this.constraints = {minx: 0, maxx: 0, miny: 0, maxy: 0};
-
+    this.dots = false;
     this.frames = 0;
     this.canvas = new CA.Canvas(aCanvasOrId);
     this.canvas.line(0, 0, this.canvas.w, this.canvas.h);
@@ -69,7 +70,7 @@ SC.ChartXYXY = function (aCanvasOrId) {
             pan = true;
             rec = t.canvas.canvas.getBoundingClientRect();
             p1 = t.canvasToReal(event.clientX - rec.x, event.clientY - rec.y);
-            //console.log('pan', p1);
+            console.log('pan', p1);
         }
     });
     window.addEventListener('mousemove', function (event) {
@@ -88,8 +89,13 @@ SC.ChartXYXY = function (aCanvasOrId) {
             //console.log('pan move', p2);
             t.minx -= p2.x - p1.x;
             t.maxx -= p2.x - p1.x;
-            t.miny -= p2.y - p1.y;
-            t.maxy -= p2.y - p1.y;
+            if (t.logY) {
+                t.miny -= Math.log10(p2.y) - Math.log10(p1.y);
+                t.maxy -= Math.log10(p2.y) - Math.log10(p1.y);
+            } else {
+                t.miny -= p2.y - p1.y;
+                t.maxy -= p2.y - p1.y;
+            }
             //p1 = p2;
             t.render();
         } else {
@@ -116,12 +122,12 @@ SC.ChartXYXY = function (aCanvasOrId) {
 SC.ChartXYXY.prototype.updateConstraints = function () {
     // update constraints
     var s, i, x, y, d;
-    this.constraints = {minx: 0, maxx: 0, miny: 0, maxy: 0};
+    this.constraints = {minx: 0, maxx: 0, miny: Number.MAX_VALUE, maxy: -Number.MAX_VALUE};
     for (s = 0; s < this.series.length; s++) {
         d = this.series[s].data;
         for (i = 1; i < d.length; i++) {
             x = d[i][0];
-            y = d[i][1];
+            y = this.logY ? Math.log10(d[i][1]) : d[i][1];
             this.constraints.minx = Math.min(this.constraints.minx, x);
             this.constraints.maxx = Math.max(this.constraints.maxx, x);
             this.constraints.miny = Math.min(this.constraints.miny, y);
@@ -152,7 +158,7 @@ SC.ChartXYXY.prototype.addSeries = function (aName, aColor, aData) {
 
 SC.ChartXYXY.prototype.resetZoomAndPan = function () {
     this.updateConstraints();
-    var bx = 0.04 * (this.constraints.maxx - this.constraints.minx),
+    var bx = (this.logY ? 0.08 : 0.04) * (this.constraints.maxx - this.constraints.minx),
         by = 0.04 * (this.constraints.maxy - this.constraints.miny);
     this.minx = this.constraints.minx - bx;
     this.miny = this.constraints.miny - by;
@@ -166,7 +172,8 @@ SC.ChartXYXY.prototype.realToCanvas = function (aX, aY, aRoundTo05) {
     var w = this.canvas.width,
         h = this.canvas.height,
         x = w * (aX - this.minx) / (this.maxx - this.minx),
-        y = h - h * (aY - this.miny) / (this.maxy - this.miny);
+        ly = this.logY ? Math.log10(aY) : aY,
+        y = h - h * (ly - this.miny) / (this.maxy - this.miny);
     if (aRoundTo05) {
         x = Math.floor(x) + 0.5;
         y = Math.floor(y) + 0.5;
@@ -180,17 +187,25 @@ SC.ChartXYXY.prototype.realToCanvas = function (aX, aY, aRoundTo05) {
 SC.ChartXYXY.prototype.canvasToReal = function (aX, aY) {
     // convert canvas coordinates to real coordinates
     var w = this.canvas.width,
-        h = this.canvas.height;
+        h = this.canvas.height,
+        x = aX * (this.maxx - this.minx) / w + this.minx,
+        y = (h - aY) * (this.maxy - this.miny) / h + this.miny;
+    if (this.logY) {
+        return {
+            x: x,
+            y: Math.pow(10, y)
+        };
+    }
     return {
-        x: aX * (this.maxx - this.minx) / w + this.minx,
-        y: (h - aY) * (this.maxy - this.miny) / h + this.miny
+        x: x,
+        y: y
     };
 };
 
 SC.ChartXYXY.prototype.render = function () {
     // Render chart
     this.frames++;
-    var i, s, ser, sd, x, y, p, a, b, x0, y0, m, strx, stry, ofs;
+    var i, s, ser, sd, x, y, p, a, b, x0, y0, m, strx, stry, ofs, ly;
     this.canvas.clear('white');
     if (!this.series) {
         return;
@@ -209,7 +224,7 @@ SC.ChartXYXY.prototype.render = function () {
     this.canvas.context.fillText('U(V)', this.canvas.width, p.y - 2);
     this.canvas.context.textAlign = 'left';
     this.canvas.context.textBaseline = 'top';
-    this.canvas.context.fillText('I(mA)', p.x + 2, 2);
+    this.canvas.context.fillText(this.logY ? 'I(A)' : 'I(mA)', p.x + 2, 2);
 
     // zero
     this.canvas.context.textAlign = 'right';
@@ -236,12 +251,39 @@ SC.ChartXYXY.prototype.render = function () {
     // y-axis
     this.canvas.context.textAlign = 'right';
     this.canvas.context.textBaseline = 'middle';
-    if ((a.y - y0) / this.gridY < 500) {
-        for (y = y0; y < a.y; y += this.gridY) {
-            p = this.realToCanvas(0, y, true);
-            if (y > 0) {
+    if (this.logY) {
+        ly = {
+            '10A': 10,
+            '1A': 1,
+            '100mA': 0.1,
+            '10mA': 0.01,
+            '1mA': 0.001,
+            '100uA': 0.0001,
+            '10uA': 0.00001,
+            '1uA': 0.000001,
+            '100nA': 0.0000001,
+            '10nA': 0.00000001,
+            '1nA': 0.000000001,
+            '100pA': 0.0000000001,
+            '10pA': 0.00000000001,
+            '1pA': 0.000000000001,
+        };
+        for (y in ly) {
+            if (ly.hasOwnProperty(y)) {
+                p = this.realToCanvas(0, ly[y], true);
                 this.canvas.line(p.x, p.y, this.canvas.width, p.y, '#00000022', 1);
-                this.canvas.context.fillText((1000 * y).toFixed(0), p.x - 1, p.y);
+                this.canvas.context.fillText(y, p.x - 1, p.y);
+            }
+        }
+    } else {
+        //console.log({ay: a.y, y0: y0, gridY: this.gridY});
+        if ((a.y - y0) / this.gridY < 500) {
+            for (y = y0; y < a.y; y += this.gridY) {
+                p = this.realToCanvas(0, y, true);
+                if (y > 0) {
+                    this.canvas.line(p.x, p.y, this.canvas.width, p.y, '#00000022', 1);
+                    this.canvas.context.fillText((1000 * y).toFixed(0), p.x - 1, p.y);
+                }
             }
         }
     }
@@ -253,6 +295,7 @@ SC.ChartXYXY.prototype.render = function () {
         ser = this.series[s];
         this.canvas.context.lineWidth = 2;
         this.canvas.context.strokeStyle = ser.color;
+        this.canvas.context.fillStyle = ser.color;
         this.canvas.context.beginPath();
         sd = ser.data;
         for (i = 0; i < sd.length; i++) {
@@ -266,6 +309,15 @@ SC.ChartXYXY.prototype.render = function () {
             }
         }
         this.canvas.context.stroke();
+        // dots
+        if (this.dots) {
+            for (i = 0; i < sd.length; i++) {
+                x = sd[i][0];
+                y = sd[i][1];
+                p = this.realToCanvas(x, y);
+                this.canvas.context.fillRect(p.x - 3, p.y - 3, 6, 6);
+            }
+        }
     }
     this.canvas.context.lineWidth = 1;
 
@@ -274,7 +326,7 @@ SC.ChartXYXY.prototype.render = function () {
         p = this.realToCanvas(this.cursor.x, this.cursor.y);
         p.x = Math.floor(p.x) + 0.5;
         p.y = Math.floor(p.y) + 0.5;
-        if (this.cursor.y >= this.miny && this.cursor.y <= this.maxy) {
+        if (this.logY || (this.cursor.y >= this.miny && this.cursor.y <= this.maxy)) {
             this.canvas.context.strokeStyle = 'black';
             // horizontal
             this.canvas.context.beginPath();
