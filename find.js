02253eb6df7a60ff6409d46f0e3f421f40e41275
Dušan Halický <dusan.halicky@gmail.com>
2022-01-24 14:20:12 +0100

    Highlighting on label hover, show score

--- find.js
+++ find.js
@@ -78,7 +78,7 @@ SC.onChangeMeasuredValues = function () {
 
 SC.findSimilar = function () {
     // Find most similar diodes to measured data
-    var k, d, candidate = [], i;
+    var k, d, candidate = [], i, palette = ['green', 'blue', 'orange', 'purple', 'brown', 'teal', 'lime'];
     for (k in SC.all) {
         if (SC.all.hasOwnProperty(k)) {
             d = SC.calculateDifference(SC.all[k].values, SC.measuredVA);
@@ -89,6 +89,10 @@ SC.findSimilar = function () {
             //console.log(k, d);
         }
     }
+
+    palette = ['green', 'blue', 'orange', 'lime'];
+    //palette = ['#007700ff', '#00ff00ff', '#00ff00cc', '#00ff00aa', '#00ff0077', '#00ff0044'];
+
     candidate.sort(function (a, b) {
         return a.avg - b.avg;
     });
@@ -97,17 +101,17 @@ SC.findSimilar = function () {
     //chart_series.color = 'green';
     for (i = 0; i < candidate.length; i++) {
         k = candidate[i].k;
-        SC.all[k].chb.checked = i < 3;
-        SC.chart1.series[SC.all[k].index].visible = i < 3;
+        SC.all[k].chb.checked = i < palette.length;
+        SC.chart1.series[SC.all[k].index].visible = i < palette.length;
         SC.chart1.series[SC.all[k].index].color = 'gray';
         SC.all[k].lab.style.color = 'black';
+        SC.all[k].score.textContent = '';
     }
-    SC.chart1.series[SC.all[candidate[0].k].index].color = 'green';
-    SC.all[candidate[0].k].lab.style.color = 'green';
-    SC.chart1.series[SC.all[candidate[1].k].index].color = 'blue';
-    SC.all[candidate[1].k].lab.style.color = 'blue';
-    SC.chart1.series[SC.all[candidate[2].k].index].color = 'cyan';
-    SC.all[candidate[2].k].lab.style.color = 'cyan';
+    for (i = 0; i < palette.length; i++) {
+        SC.chart1.series[SC.all[candidate[i].k].index].color = palette[i];
+        SC.all[candidate[i].k].lab.style.color = palette[i];
+        SC.all[candidate[i].k].score.textContent = ' #' + (i + 1);
+    }
     SC.chart1.render();
 };
 
@@ -146,6 +150,19 @@ SC.onClear = function () {
     SC.e.measured_values.value = '';
 };
 
+SC.onHoverLabel = function (event) {
+    // Highlight hovered label
+    var k = event.target.dataKey,
+        i;
+    // clear previous highlighting
+    for (i = 0; i < SC.chart1.series.length; i++) {
+        SC.chart1.series[i].highlight = false;
+    }
+    // highlight current
+    SC.chart1.series[SC.all[k].index].highlight = true;
+    SC.chart1.render();
+};
+
 window.addEventListener('DOMContentLoaded', function () {
     // init
     SC.e = CA.elements('measured_values', 'zoom_to_fit', 'logy', 'diodes',
@@ -167,7 +184,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // add known diodes
     SC.all = {};
-    var k, lab, chb, i;
+    var k, lab, chb, i, score;
     for (k in SC.realDiode) {
         if (SC.realDiode.hasOwnProperty(k)) {
             i = SC.chart1.addSeries(k, 'gray', SC.realDiode[k]);
@@ -175,6 +192,7 @@ window.addEventListener('DOMContentLoaded', function () {
             // label
             lab = document.createElement('label');
             lab.dataKey = k;
+            lab.onmouseover = SC.onHoverLabel;
             chb = document.createElement('input');
             chb.dataKey = k;
             chb.dataIndex = i;
@@ -183,12 +201,17 @@ window.addEventListener('DOMContentLoaded', function () {
             chb.onclick = SC.onClickDiode;
             lab.appendChild(chb);
             lab.appendChild(document.createTextNode(k));
+            score = document.createElement('span');
+            score.dataKey = k;
+            score.textContent = '';
+            lab.appendChild(score);
             SC.e.diodes.appendChild(lab);
             SC.all[k] = {
                 index: i,
                 key: k,
                 chb: chb,
                 lab: lab,
+                score: score,
                 values: SC.realDiode[k]
             };
         }
